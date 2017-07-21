0a0a22abac7b00b1b46ced26de4fe3b055342c44
Daniel Malone <danieljmalone@gmail.com>
2017-07-21 15:47:16 -0500

    make face detection work

--- app/src/main/java/com/finepointmobile/facedetectionapp/MainActivity.java
+++ app/src/main/java/com/finepointmobile/facedetectionapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.finepointmobile.facedetectionapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FaceOverlayView mFaceOverlayView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +22,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        mFaceOverlayView = (FaceOverlayView) findViewById(R.id.face_overlay);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        InputStream stream = getResources().openRawResource(R.raw.face);
+        Bitmap bitmap = BitmapFactory.decodeStream(stream);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        mFaceOverlayView.setBitmap(bitmap);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setMinFaceSize(FaceDetector.FAST_MODE)
+                .build();
     }
 }

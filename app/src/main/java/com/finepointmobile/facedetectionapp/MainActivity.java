c5525008eb2f669605ada75524b0d35f43c304b0
Daniel Malone <danieljmalone@gmail.com>
2017-07-21 19:56:07 -0500

    make permissions work

--- app/src/main/java/com/finepointmobile/facedetectionapp/MainActivity.java
+++ app/src/main/java/com/finepointmobile/facedetectionapp/MainActivity.java
@@ -1,19 +1,29 @@
 package com.finepointmobile.facedetectionapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
-
-import com.google.android.gms.vision.face.FaceDetector;
+import android.util.Log;
+import android.view.View;
 
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private FaceOverlayView mFaceOverlayView;
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +33,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mFaceOverlayView = (FaceOverlayView) findViewById(R.id.face_overlay);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
 
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int cameraPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+                if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 200);
+                    Log.d(TAG, "onClick: show permission dialog");
+                } else {
+                    openCamera();
+                }
+            }
+        });
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 200);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult: " + requestCode);
+        if (requestCode == 200 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        }
+    }
+
+    private void doFaceRecognition() {
         InputStream stream = getResources().openRawResource(R.raw.face);
         Bitmap bitmap = BitmapFactory.decodeStream(stream);
 
         mFaceOverlayView.setBitmap(bitmap);
-
-        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
-                .setTrackingEnabled(false)
-                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
-                .setMinFaceSize(FaceDetector.FAST_MODE)
-                .build();
     }
 }
